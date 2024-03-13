62fc714e0fa155271344b523c0e45bd95abadd91
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2024-03-13 22:24:47 +0530

    Update settings.py

--- soaialmedia/settings.py
+++ soaialmedia/settings.py
@@ -164,7 +164,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'grdfaiyaz786@gmail.com'
-EMAIL_HOST_PASSWORD = 'moxx ftlr jmnt tudd'
+EMAIL_HOST_PASSWORD = 'moxx 
 
 
 SIMPLE_JWT = {
