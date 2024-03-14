d615391cb41a9d3c3f06e3c25f95774d02362760
faiyazAziz <106823454+faiyazAziz@users.noreply.github.com>
2024-03-14 16:18:43 +0530

    Update settings.py

--- soaialmedia/settings.py
+++ soaialmedia/settings.py
@@ -164,8 +164,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'grdfaiyaz786@gmail.com'
-EMAIL_HOST_PASSWORD = 'moxx 
-
+EMAIL_HOST_PASSWORD = 'moaa'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME':timedelta(days=15),
